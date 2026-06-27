fa6c7e9e91985ebff876c43eed4f6bf6961256a3
SunnyAero3 <jake.hili3231@gmail.com>
2026-06-27 16:47:24 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,17 +1,17 @@
 import { supabase } from "./supabase.js";
 
-async function login() {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+window.login = async function() {
+  const email = email.value;
+  const password = password.value;
 
-  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  await supabase.auth.signInWithPassword({ email, password });
 
-  if (!error) window.location.href = "home.html";
-}
+  window.location.href = "home.html";
+};
 
-async function signup() {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+window.signup = async function() {
+  const email = email.value;
+  const password = password.value;
 
   await supabase.auth.signUp({ email, password });
-}
+};
